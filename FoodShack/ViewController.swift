ce397b218c562e0951c5db7ceb070812f23188da
Bruce <bruce@redravencomputing.com>
2016-11-26 14:57:05 -0700

    revert back to legacy swift

--- FoodShack/ViewController.swift
+++ FoodShack/ViewController.swift
@@ -16,11 +16,28 @@ class ViewController: UIViewController {
     let tacoBtn = UIButton()
     let orderNowBtn = UIButton()
     let selectedItemThumbmg = UIImageView(image: UIImage(named: "burger"))
+    let topViewHolder = UIView()
+    let mainThumbViewHolder = UIView()
+    let buttonViewHolder  = UIView()
+    let bottomViewHolder = UIView()
+    let topThumbBtn = UIButton()
+    let topTitleLbl = UILabel()
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = UIColor.purpleColor()
+        self.topViewHolder.backgroundColor = UIColor.whiteColor()
+        self.mainThumbViewHolder.backgroundColor = UIColor(red: 220.0/255.0, green: 224.0/255.0, blue: 210.0/255.0, alpha: 1.0)
+        self.buttonViewHolder.backgroundColor = UIColor(red: 242.0/255.0, green: 241.0/255.0, blue: 239.0/255.0, alpha: 1.0)
+        self.bottomViewHolder.backgroundColor = UIColor(red: 39.0/255.0, green: 61.0/255.0, blue: 72.0/255.0, alpha: 1.0)
+        
+        self.view.addSubview(topViewHolder)
+        self.view.addSubview(mainThumbViewHolder)
+        self.view.addSubview(buttonViewHolder)
+        self.view.addSubview(bottomViewHolder)
+        
     }
 
 }

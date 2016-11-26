ecb592ef360a1bba82d0158d33fda2883a7b6dff
Bruce <bruce@redravencomputing.com>
2016-11-26 15:21:43 -0700

    Add first constraints programmatically

--- FoodShack/ViewController.swift
+++ FoodShack/ViewController.swift
@@ -23,6 +23,9 @@ class ViewController: UIViewController {
     let topThumbBtn = UIButton()
     let topTitleLbl = UILabel()
     
+    var views = Dictionary<String, AnyObject>()
+    var constraints = [NSLayoutConstraint]()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,10 +36,41 @@ class ViewController: UIViewController {
         self.buttonViewHolder.backgroundColor = UIColor(red: 242.0/255.0, green: 241.0/255.0, blue: 239.0/255.0, alpha: 1.0)
         self.bottomViewHolder.backgroundColor = UIColor(red: 39.0/255.0, green: 61.0/255.0, blue: 72.0/255.0, alpha: 1.0)
         
+        self.topViewHolder.translatesAutoresizingMaskIntoConstraints = false
+        self.mainThumbViewHolder.translatesAutoresizingMaskIntoConstraints = false
+        self.buttonViewHolder.translatesAutoresizingMaskIntoConstraints = false
+        self.bottomViewHolder.translatesAutoresizingMaskIntoConstraints = false
+        
         self.view.addSubview(topViewHolder)
         self.view.addSubview(mainThumbViewHolder)
         self.view.addSubview(buttonViewHolder)
         self.view.addSubview(bottomViewHolder)
+        
+        self.views["topViewHolder"] = topViewHolder
+        self.views["mainThumbViewHolder"] = mainThumbViewHolder
+        self.views["buttonViewHolder"] = buttonViewHolder
+        self.views["bottomViewHolder"] = bottomViewHolder
+        
+        setConstraints()
+        
+    }
+    
+    func setConstraints() {
+        
+        /** TOP VIEW HOLDER **/
+        var formatString = "V:|-[topViewHolder(50)]"
+        
+        var newConstraints = NSLayoutConstraint.constraintsWithVisualFormat(formatString, options: [], metrics: nil, views: self.views)
+        
+        self.constraints += newConstraints
+        
+        formatString = "H:|-0-[topViewHolder]-0-|"
+        
+        newConstraints = NSLayoutConstraint.constraintsWithVisualFormat(formatString, options: [], metrics: nil, views: self.views)
+        
+        self.constraints += newConstraints
+        
+        NSLayoutConstraint.activateConstraints(self.constraints)
         
     }
 

99ce4a098c79570ea6d1f3bae28fb19ad097e2b0
Bruce <bruce@redravencomputing.com>
2016-11-26 13:37:23 -0700

    initial commit

--- FoodShack/ViewController.swift
+++ FoodShack/ViewController.swift
@@ -9,17 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let burgerBtn = UIButton()
+    let pizzaBtn = UIButton()
+    let sandwichBtn = UIButton()
+    let tacoBtn = UIButton()
+    let orderNowBtn = UIButton()
+    let selectedItemThumbmg = UIImageView(image: UIImage(named: "burger"))
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 

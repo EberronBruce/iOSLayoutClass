6b6d87922b5e25d146fa7d6625c65bfd84b8638d
Bruce <bruce@redravencomputing.com>
2016-11-27 00:07:35 -0700

    Added images and more constraints

--- FoodShack/ViewController.swift
+++ FoodShack/ViewController.swift
@@ -36,20 +36,31 @@ class ViewController: UIViewController {
         self.buttonViewHolder.backgroundColor = UIColor(red: 242.0/255.0, green: 241.0/255.0, blue: 239.0/255.0, alpha: 1.0)
         self.bottomViewHolder.backgroundColor = UIColor(red: 39.0/255.0, green: 61.0/255.0, blue: 72.0/255.0, alpha: 1.0)
         
+        self.topThumbBtn.setImage(UIImage(named:"burger"), forState: .Normal)
+        self.topTitleLbl.text = "FOOD SHACK"
+        self.topTitleLbl.textColor = UIColor(red: 39.0/255.0, green: 61.0/255.0, blue: 72.0/255.0, alpha: 1.0)
+        
         self.topViewHolder.translatesAutoresizingMaskIntoConstraints = false
         self.mainThumbViewHolder.translatesAutoresizingMaskIntoConstraints = false
         self.buttonViewHolder.translatesAutoresizingMaskIntoConstraints = false
         self.bottomViewHolder.translatesAutoresizingMaskIntoConstraints = false
+        self.topThumbBtn.translatesAutoresizingMaskIntoConstraints = false
+        self.topTitleLbl.translatesAutoresizingMaskIntoConstraints = false
         
         self.view.addSubview(topViewHolder)
         self.view.addSubview(mainThumbViewHolder)
         self.view.addSubview(buttonViewHolder)
         self.view.addSubview(bottomViewHolder)
         
+        self.topViewHolder.addSubview(self.topThumbBtn)
+        self.topViewHolder.addSubview(self.topTitleLbl)
+        
         self.views["topViewHolder"] = topViewHolder
         self.views["mainThumbViewHolder"] = mainThumbViewHolder
         self.views["buttonViewHolder"] = buttonViewHolder
         self.views["bottomViewHolder"] = bottomViewHolder
+        self.views["topThumbBtn"] = topThumbBtn
+        self.views["topTitleLbl"] = topTitleLbl
         
         setConstraints()
         
@@ -58,21 +69,41 @@ class ViewController: UIViewController {
     func setConstraints() {
         
         /** TOP VIEW HOLDER **/
-        var formatString = "V:|-[topViewHolder(50)]"
+        addConstraint("V:|-[topViewHolder(50)]-0-[mainThumbViewHolder]")
+        addConstraint("H:|-0-[topViewHolder]-0-|")
         
-        var newConstraints = NSLayoutConstraint.constraintsWithVisualFormat(formatString, options: [], metrics: nil, views: self.views)
+        /** MAIN VIEW HOLDER **/
+        addConstraint("V:[mainThumbViewHolder(<=250@250)]-0-[buttonViewHolder]")
+        addConstraint("V:[mainThumbViewHolder(>=150@250)]")
+        addConstraint("H:|-0-[mainThumbViewHolder(==topViewHolder)]-0-|")
         
-        self.constraints += newConstraints
+        /** BUTTON VIEW HOLDER **/
+        addConstraint("V:[buttonViewHolder(<=200)]-0-[bottomViewHolder]")
+        addConstraint("V:[buttonViewHolder(>=100)]")
+        addConstraint("H:|-0-[buttonViewHolder(mainThumbViewHolder)]-0-|")
         
-        formatString = "H:|-0-[topViewHolder]-0-|"
+        //** BOTTOM VIEW HOLDER **/
+        addConstraint("V:[bottomViewHolder(50@1000)]-|")
+        addConstraint("H:|-0-[bottomViewHolder(buttonViewHolder)]-0-|")
         
-        newConstraints = NSLayoutConstraint.constraintsWithVisualFormat(formatString, options: [], metrics: nil, views: self.views)
+        //** TOP THUMB BUTTON **/
+        addConstraint("V:|-5-[topThumbBtn(40)]")
+        addConstraint("H:|-5-[topThumbBtn(40)]-10-[topTitleLbl]")
         
-        self.constraints += newConstraints
+        //** TOP TITLE LABLE **/
+        addConstraint("V:|-5-[topTitleLbl(40)]")
+        addConstraint("H:[topTitleLbl(>=50)]")
+
         
         NSLayoutConstraint.activateConstraints(self.constraints)
         
     }
-
+    
+    func addConstraint(format: String) {
+        
+        let newConstraints = NSLayoutConstraint.constraintsWithVisualFormat(format, options: [], metrics: nil, views: self.views)
+        
+        self.constraints += newConstraints
+    }
 }
 
